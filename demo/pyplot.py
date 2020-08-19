0d8e1f4e3d568cb4fc10f56fb4e4107dc57938a8
David Broman <dbro@kth.se>
2020-08-19 15:10:48 +0200

    Updated plotting window texts.

--- demo/pyplot.py
+++ demo/pyplot.py
@@ -1,6 +1,7 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
 
 msft = pd.read_csv('mozplot.dat', sep="\t")
 header = msft.columns.values
@@ -9,5 +10,6 @@ for y in header[1:-1]:
     ax.plot(msft['time'],msft[y], label=y)
 
 ax.legend()
-fig.suptitle('Modelyze plot')
+fig.suptitle(sys.argv[1])
+fig.canvas.set_window_title('')
 plt.show()
